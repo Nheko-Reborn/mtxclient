e9d0297aa14dc0b00526693413da71bda4f66f45
Konstantinos Sideris <sideris.konstantin@gmail.com>
2018-02-27 21:12:54 +0200

    Update ci

---
+++ .ci/boost.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -evx
+
+sudo wget -O boost_1_66_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.66.0/boost_1_66_0.tar.gz/download
+sudo tar xzf boost_1_66_0.tar.gz
+
+cd boost_1_66_0/
+sudo ./bootstrap.sh --with-libraries=random,thread,system,chrono,date_time,atomic --prefix=/usr/local
+sudo ./b2 -d0 variant=debug link=shared
+sudo ./b2 -d0 install 
+
