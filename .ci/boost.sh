6c77be4781ed6f3c616c1a4ed795543bd52a5b75
Konstantinos Sideris <sideris.konstantin@gmail.com>
2018-02-27 21:42:01 +0200

    Add pthread to cmakelists

--- .ci/boost.sh
+++ .ci/boost.sh
@@ -6,7 +6,7 @@ sudo wget -O boost_1_66_0.tar.gz http://sourceforge.net/projects/boost/files/boo
 sudo tar xzf boost_1_66_0.tar.gz
 
 cd boost_1_66_0/
-sudo ./bootstrap.sh --with-libraries=random,thread,system,chrono,date_time,atomic --prefix=/usr/local
+sudo ./bootstrap.sh --with-libraries=random,thread,system --prefix=/usr/local
 sudo ./b2 -d0 variant=debug link=shared
 sudo ./b2 -d0 install 
 
