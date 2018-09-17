c4806522358e51a9ecd0ea6648e19b413eaeec07
Konstantinos Sideris <sideris.konstantin@gmail.com>
2018-09-17 21:02:54 +0300

    Install libsodium from source

--- .ci/install.sh
+++ .ci/install.sh
@@ -12,10 +12,19 @@ if [ $TRAVIS_OS_NAME == linux ]; then
     export CXX=${CXX_VERSION}
     export CC=${CC_VERSION}
 
-    sudo add-apt-repository -y ppa:chris-lea/libsodium
     sudo apt-get update -qq
-    sudo apt-get install -qq -y libsodium-dev
     sudo apt-get remove -y cmake
+
+    # Build & install libsodium for source.
+    mkdir -p libsodium && pushd libsodium
+    curl -L \
+        https://download.libsodium.org/libsodium/releases/libsodium-${LIBSODIUM_VERSION}.tar.gz \
+        -o libsodium-${LIBSODIUM_VERSION}.tar.gz
+    tar xfz libsodium-${LIBSODIUM_VERSION}.tar.gz
+    pushd libsodium-${LIBSODIUM_VERSION}/
+    ./configure && make && make check && make install         
+    popd
+    popd
 
     wget https://cmake.org/files/v3.11/cmake-3.11.4-Linux-x86_64.sh
     sudo sh cmake-3.11.4-Linux-x86_64.sh  --skip-license  --prefix=/usr/local
