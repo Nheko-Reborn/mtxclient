8f4b4610a624de48136be63d47091d97d1dfe602
Nicolas Werner <nicolas.werner@hotmail.de>
2019-12-12 02:23:14 +0100

    Don't run build in docker

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,6 +2,45 @@
 
 set -ex
 
+CMAKE_VERSION=3.15.5
+CMAKE_SHORT_VERSION=3.15
+
+if [ $TRAVIS_OS_NAME == linux ]; then
+    sudo apt-get update -qq
+    sudo apt-get install -y --no-install-recommends apt-transport-https software-properties-common curl ninja-build
+    # cmake
+    curl https://cmake.org/files/v${CMAKE_SHORT_VERSION}/cmake-${CMAKE_VERSION}-Linux-x86_64.sh -o cmake-install.sh
+    sudo bash cmake-install.sh --skip-license --prefix=/usr/local
+    export PATH="/usr/local/bin:$PATH"
+
+    # Toolchains
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test && \
+    curl -L https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+    sudo apt-add-repository "deb https://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main"
+    sudo apt-get update -qq
+    sudo apt-get install -y --no-install-recommends \
+        pkg-config \
+        make \
+        clang++-6.0 \
+        clang-6.0 \
+        g++-8 \
+        unzip git \
+        libssl-dev \
+        openssl
+
+    mkdir -p build-libsodium
+    ( cd build-libsodium
+      curl -L https://download.libsodium.org/libsodium/releases/libsodium-1.0.17.tar.gz -o libsodium-1.0.17.tar.gz
+      tar xfz libsodium-1.0.17.tar.gz
+      cd libsodium-1.0.17/
+      ./configure && make && sudo make install )
+    mkdir -p build-lcov
+    ( cd build-lcov
+      curl -L http://downloads.sourceforge.net/ltp/lcov-1.14.tar.gz -o lcov-1.14.tar.gz
+      tar xfz lcov-1.14.tar.gz
+      cd lcov-1.14/
+      sudo make install )
+fi
 if [ $TRAVIS_OS_NAME == osx ]; then
     brew update || true
     brew upgrade boost || true
--- .ci/script.sh
+++ .ci/script.sh
@@ -5,6 +5,9 @@ set -ex
 if [ $TRAVIS_OS_NAME == linux ]; then
     export CXX=${CXX_VERSION}
     export CC=${CC_VERSION}
+    export PATH="/usr/local/bin:$PATH"
+
+    cmake --version
 
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/${CC_VERSION} 10
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/${CXX_VERSION} 10
@@ -14,17 +17,20 @@ if [ $TRAVIS_OS_NAME == linux ]; then
     sudo update-alternatives --set g++ "/usr/bin/${CXX_VERSION}"
     sudo update-alternatives --set gcov "/usr/bin/gcov-8"
 
+    # Build dependencies.
+    cmake -GNinja -Hdeps -B.deps -DCMAKE_BUILD_TYPE=Debug
+    cmake --build .deps
+
     # Build the library.
-    cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug \
+    cmake -GNinja -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug \
         -DBUILD_LIB_TESTS=ON \
         -DBUILD_SHARED_LIBS=ON \
+	-DCMAKE_INSTALL_PREFIX=.deps/usr \
         -DCOVERAGE=${COVERAGE} || true
     cmake --build build
 
     # The tests will run anyway during coverage.
-    if [ $COVERAGE != ON ]; then
-        make test
-    fi
+    make test
 fi
 
 if [ $TRAVIS_OS_NAME == osx ]; then
