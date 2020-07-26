5eaac70157da5491093d3e09979031229a6e55c7
Nicolas Werner <nicolas.werner@hotmail.de>
2020-07-26 12:14:43 +0200

    Fix lint of overloaded

--- lib/structs/user_interactive.cpp
+++ lib/structs/user_interactive.cpp
@@ -62,7 +62,7 @@ struct overloaded : Ts...
         using Ts::operator()...;
 };
 template<class... Ts>
-overloaded(Ts...)->overloaded<Ts...>;
+overloaded(Ts...) -> overloaded<Ts...>;
 }
 
 namespace auth {
