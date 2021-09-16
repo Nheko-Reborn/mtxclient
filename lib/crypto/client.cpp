b452a984b0fc522c21bb8df7d320bf13960974d0
Nicolas Werner <nicolas.werner@hotmail.de>
2021-09-16 15:51:33 +0200

    Fix compilaton with Olm 3.2.5

--- lib/crypto/client.cpp
+++ lib/crypto/client.cpp
@@ -37,15 +37,15 @@ static const std::array olmErrorStrings{
 
 };
 
-OlmErrorCode
+mtx::crypto::OlmErrorCode
 olm_exception::ec_from_string(std::string_view error)
 {
     for (size_t i = 0; i < olmErrorStrings.size(); i++) {
         if (olmErrorStrings[i] == error)
-            return static_cast<OlmErrorCode>(i);
+            return static_cast<mtx::crypto::OlmErrorCode>(i);
     }
 
-    return OlmErrorCode::UNKNOWN_ERROR;
+    return mtx::crypto::OlmErrorCode::UNKNOWN_ERROR;
 }
 
 void
