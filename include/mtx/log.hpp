7554358097db99d0ad7a6abd4b5b709bd917aa02
Nicolas Werner <nicolas.werner@hotmail.de>
2022-10-21 22:46:29 +0200

    Fix doc typo

--- include/mtx/log.hpp
+++ include/mtx/log.hpp
@@ -12,8 +12,8 @@ namespace utils {
 namespace log {
 /// @brief Access the logger and modify it.
 ///
-/// For example you can set the sinky using `mtx::utils::log::log()->sinks() = sinks` or modify the
-/// loglevel using `mtx::utils::log::log()->set_level(spdlog::level::trace)`
+/// For example you can set the sinks using `mtx::utils::log::log()->sinks() = sinks` or modify the
+/// loglevel using `mtx::utils::log::log()->set_level(spdlog::level::trace)`.
 std::shared_ptr<spdlog::logger>
 log();
 }
