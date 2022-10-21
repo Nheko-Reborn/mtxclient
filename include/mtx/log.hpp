0ba5c257bda481f4e628d92d2d6469f79f447dad
Nicolas Werner <nicolas.werner@hotmail.de>
2022-10-21 22:40:36 +0200

    Document the log() function

--- include/mtx/log.hpp
+++ include/mtx/log.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
+/// @file
+/// @brief Header for logging related functions
+
 #include <memory>
 #include <spdlog/spdlog.h>
 
 namespace mtx {
 namespace utils {
+//! Logger utilities
 namespace log {
+/// @brief Access the logger and modify it.
+///
+/// For example you can set the sinky using `mtx::utils::log::log()->sinks() = sinks` or modify the
+/// loglevel using `mtx::utils::log::log()->set_level(spdlog::level::trace)`
 std::shared_ptr<spdlog::logger>
 log();
 }
